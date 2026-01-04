ca086e068af6d29c64952cbf622e7ddc4086e022
ruchitha <ruchitha.k2007@gmail.com>
2026-01-04 13:57:27 +0530

    Fix Express wildcard route for Node 22

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.options('*', (req, res) => {
-    res.sendStatus(204);
-  });
 
 app.use(express.json());
 app.use(cookieParser()); // Added cookie-parser middleware
