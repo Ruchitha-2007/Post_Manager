40664eafbb99fd3c47295c6a5934926721928974
Ruchitha-2007 <ruchitha.k2007@gmail.com>
2026-01-05 19:48:11 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, // ✅ THIS is required
+    baseURL:'https://ruchi-post-manager.onrender.com', 
     withCredentials: true,
 });
 
