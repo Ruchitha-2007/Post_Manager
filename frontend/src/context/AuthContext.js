7903bd790e850f0cb07581af007e1937300bc7b7
Ruchitha-2007 <ruchitha.k2007@gmail.com>
2026-01-05 21:18:19 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -6,6 +6,30 @@ export const AuthProvider = ({ children }) => {
     const [name,setName]=useState('');
     const [loggedIn,setLoggedIn]=useState(false);
     const [isEditing,setIsEditing]=useState(false)
+    const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const restoreSession = async () => {
+      try {
+        const res = await api.get('/auth/refresh');
+        const { accessToken, user } = res.data;
+
+        localStorage.setItem('accessToken', accessToken);
+        setName(user.name);
+        setLoggedIn(true);
+      } catch (err) {
+        setName('');
+        setLoggedIn(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    restoreSession();
+  }, []);
+
+  if (loading) return null; 
+
 
     return (
         <AuthContext.Provider value={{ 
